ea7a53e82abdb3bd180341a5f5af8b00a0c7d58a
mahesh <mahesh2885858@gmail.com>
2022-03-24 13:45:47 +0530

    final commit i think

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import { useState, useRef } from "react";
+import { v4 as uuid } from "uuid";
 import "./app.scss";
 function App() {
+  type todoitem = {
+    todo: string;
+    id: string;
+  };
   const [showModal, setShowModal] = useState(false);
-  const inputref = useRef<null | HTMLInputElement>(null);
-  const [todo, setTodos] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+  const [todo, setTodos] = useState<todoitem[]>([]);
   const openModal = () => {
     setShowModal(true);
   };
   const addTodo = () => {
-    const item = inputref.current?.value;
+    const item = { todo: input, id: uuid() };
     setTodos((x: string[] | any) => {
       return [...x, item];
     });
+    setInput("");
+    setShowModal(false);
   };
   return (
     <div className="App">
       <h1>Modal</h1>
       {todo.map((item) => {
-        return <li>{item}</li>;
+        return <li key={item.id}>{item.todo}</li>;
       })}
       <div id="modal" className={showModal ? "modal-show" : "hide-modal"}>
         <div className="modal-content">
-          <input ref={inputref} type="text" />
+          <input
+            value={input}
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+          />
           <button onClick={addTodo}>Add Todo</button>
           <button onClick={() => setShowModal(false)}>Close</button>
         </div>
